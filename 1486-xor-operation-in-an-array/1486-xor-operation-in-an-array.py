5be8e04db59317d04e4949a9687565a6b0454cdf
Harsh Bhandari <harshbhandari32@gmail.com>
2022-06-09 16:33:45 +0530

    Time: 48 ms (38.35%), Space: 13.8 MB (60.02%) - LeetHub

---
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def xorOperation(self, n: int, start: int) -> int:
+        nums = []
+        for i in range(n):
+            nums.append(start + (2 * i))
+            
+        res = nums[0]
+        for i in range(1, n):
+            res = res ^ nums[i]
+        
+        return res
