2669bcbbb6dea32f824c3064e89cb0085e576b75
Harsh Bhandari <harshbhandari32@gmail.com>
2022-06-09 16:55:00 +0530

    Time: 253 ms (25.65%), Space: 16.8 MB (49.81%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,17 +1,8 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         leng = len(nums)
-        if leng == 1:
-            return nums[0]
+        res = nums[0]
+        for i in range(1, leng):
+            res = res ^ nums[i]
         
-        nums_hash = {}
-        for ele in nums:
-            if ele in nums_hash:
-                nums_hash[ele] += 1
-            else:
-                nums_hash[ele] = 1
-                
-                
-        for ele in nums:
-            if nums_hash[ele] == 1:
-                return ele
+        return res
