8f40c951921fadd26bfaa7c0e84a7eb3252a23cf
Harsh Bhandari <harshbhandari32@gmail.com>
2022-07-03 09:34:17 +0530

    Time: 49 ms (90.91%), Space: 13.9 MB (72.73%) - LeetHub

--- 2325-decode-the-message/2325-decode-the-message.py
+++ 2325-decode-the-message/2325-decode-the-message.py
@@ -1,17 +1,15 @@
 class Solution:
     def decodeMessage(self, key: str, message: str) -> str:
-        key_map = {}
+        key_map = { ' ': ' ' }
         ch = 97
         decoded = ''
         for s in key:
-            if s not in key_map and s != ' ':
+            if s not in key_map:
                 key_map[s] = chr(ch)
                 ch += 1
         
         for s in message:
             if s in key_map:
                 decoded += key_map[s]
-            if s == ' ':
-                decoded += ' '
         
         return decoded
