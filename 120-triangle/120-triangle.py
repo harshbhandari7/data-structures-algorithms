3abb7779c73b13594182cdc7afb53c55bedd87c7
Harsh Bhandari <harshbhandari32@gmail.com>
2022-07-19 10:33:08 +0530

    Time: 176 ms (5.12%), Space: 17 MB (12.75%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -1,21 +1,19 @@
 class Solution:
-    def get_sum(self, i, j, grid, memo, mini):
+    def get_sum(self, i, j, grid, memo):
       if i == len(grid) - 1:
         return grid[i][j]
       
       if (i, j) in memo:
         return memo[(i, j)]
       
-      bottom = self.get_sum(i+1, j, grid, memo, mini)
-      right = self.get_sum(i+1, j+1, grid, memo, mini)
+      bottom = grid[i][j] + self.get_sum(i+1, j, grid, memo)
+      right = grid[i][j] + self.get_sum(i+1, j+1, grid, memo)
       
-      curr = grid[i][j] + min(bottom, right)
-      print('curr-', curr)
-      mini += curr
-      memo[(i, j)] = mini
-      return mini
+      curr = min(bottom, right)
+
+      memo[(i, j)] = curr
+      return curr
       
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         memo = {}
-        print('sytart')
-        return self.get_sum(0, 0, triangle, memo, 0)
+        return self.get_sum(0, 0, triangle, memo)
