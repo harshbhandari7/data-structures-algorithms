c77d2f7c6aa9cfe04094f00ed1cc98ff29379baf
Harsh Bhandari <harshbhandari32@gmail.com>
2022-07-25 21:22:03 +0530

    Time: 63 ms (28.23%), Space: 14.3 MB (26.00%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
@@ -1,3 +1,4 @@
+from collections import deque
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -5,20 +6,29 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def get_node(self, node, nodes_hash, level):
-        if node:
-            if level not in nodes_hash:
-                nodes_hash[level] = []
-            nodes_hash[level].append(node.val)
-            
-            self.get_node(node.left, nodes_hash, level+1)
-            self.get_node(node.right, nodes_hash, level+1)
-        
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if not root:
-            return []
+        if root is None:
+          return []
         
-        nodes_hash = {}
-        self.get_node(root, nodes_hash, 0)
-        return [nodes_hash[i] for i in nodes_hash.keys()]
+        level_order = []
+        queue = deque()
+        curr_level = 0
         
+        queue.append(root)
+        
+        while queue:
+          level_order.append([])
+          
+          for i in range(len(queue)):
+            node = queue.popleft()
+            
+            level_order[curr_level].append(node.val)
+            if node.left:
+              queue.append(node.left)
+
+            if node.right:
+              queue.append(node.right)
+          
+          curr_level += 1
+        
+        return level_order
