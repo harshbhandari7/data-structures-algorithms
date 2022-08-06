c02ed9c8332c404239b9e4dcbabb06ab91b02b43
Harsh Bhandari <harshbhandari32@gmail.com>
2022-08-06 09:52:49 +0530

    Time: 49 ms (65.55%), Space: 13.8 MB (59.14%) - LeetHub

--- 747-largest-number-at-least-twice-of-others/747-largest-number-at-least-twice-of-others.py
+++ 747-largest-number-at-least-twice-of-others/747-largest-number-at-least-twice-of-others.py
@@ -1,12 +1,18 @@
 class Solution:
     def dominantIndex(self, nums: List[int]) -> int:
         leng = len(nums)
-        max_num, max_num_ind = nums[0], 0
-        for i in range(1, leng):
-            if (nums[i] > max_num):
-                max_num = nums[i]
-                max_num_ind = i
+        ind = -1
+        maxi = float('-inf')
         for i in range(leng):
-            if (i != max_num_ind and max_num < 2*nums[i]):
-                return -1
-        return max_num_ind
+          if nums[i] > maxi:
+            maxi = nums[i]
+            ind = i
+        
+        for i in range(leng):
+          if nums[i] != maxi and 2 * nums[i] > maxi:
+            return -1
+        
+        return ind
+        
+        
+          
