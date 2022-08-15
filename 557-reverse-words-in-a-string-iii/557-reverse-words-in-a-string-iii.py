18fff03d1de5f2b41cf69da8dfd948380dd5aa68
Harsh Bhandari <harshbhandari32@gmail.com>
2022-08-15 13:37:07 +0530

    Time: 53 ms (69.47%), Space: 14.5 MB (82.17%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.py
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.py
@@ -4,14 +4,6 @@ class Solution:
         sn = s.split()
         leng = len(sn)
         for i in range(leng):
-            snc = [None] * len(sn[i])
-            start = 0
-            end = len(snc) - 1
-            while start <= end:
-                snc[start], snc[end] = sn[i][end], sn[i][start]
-                start += 1
-                end -= 1
-                
-            res.append(''.join(snc))
-        
+            res.append(sn[i][::-1])
+            
         return ' '.join(res)
