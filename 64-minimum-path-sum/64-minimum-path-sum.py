bcfd7619038267bc1e96221d9237feea9a948f63
Harsh Bhandari <harshbhandari32@gmail.com>
2022-10-03 08:29:24 +0530

    Time: 312 ms (5.59%), Space: 19.9 MB (9.30%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.py
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -1,22 +1,23 @@
 class Solution:
-    def get_sum(self, i, j, grid, memo):
-      if i == 0 and j == 0:
-        return grid[i][j]
-      
-      if i < 0 or j < 0:
-        return float('inf')
-      
-      if (i, j) in memo:
-        return memo[(i, j)]
-      
-      up = grid[i][j] + self.get_sum(i-1, j, grid, memo)
-      left = grid [i][j] + self.get_sum(i, j-1, grid, memo)
-      mini = min(up, left)
-      memo[(i, j)] = mini
-      return mini
-      
+    def get_path(self, i, j, grid, memo):
+        if i == 0 and j == 0:
+          return grid[0][0]
+        
+        if i < 0 or j < 0:
+          return float('inf')
+          
+        if (i, j) in memo:
+          return memo[(i, j)]
+        
+        up = grid[i][j] + self.get_path(i-1, j, grid, memo)
+        left = grid[i][j] + self.get_path(i, j-1, grid, memo)
+        mini = min(up, left)
+        memo[(i, j)] = mini
+        return mini
+          
     def minPathSum(self, grid: List[List[int]]) -> int:
         memo = {}
-        r = len(grid)
-        c = len(grid[0])
-        return self.get_sum(r-1, c-1, grid, memo)
+        m = len(grid)
+        n = len(grid[0])
+        
+        return self.get_path(m-1, n-1, grid, memo)
